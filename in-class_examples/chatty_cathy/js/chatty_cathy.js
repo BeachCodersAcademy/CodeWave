976004ec87aaa41e22ca58b3e50329c63d74a355
Christoph Anderson <cwolfanderson@gmail.com>
2016-08-24 11:55:45 -0700

    Add Firebase functionality

--- in-class_examples/chatty_cathy/js/chatty_cathy.js
+++ in-class_examples/chatty_cathy/js/chatty_cathy.js
@@ -1,16 +1,28 @@
 $(function() {
   
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBaVHQRyEEeot5gJlGaGmCyDgy2_-RePjQ",
+    authDomain: "chatty-cathy-6136e.firebaseapp.com",
+    databaseURL: "https://chatty-cathy-6136e.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+  
+  // initialize database
+  let database = firebase.database();
   
   $('#chat-btn').click(function() {
-
+    
     let $chat = $('textarea');        
     let chat = $chat.val();        
     $chat.val('');
     
+    // put cursor back in textarea
     $chat.focus();
-
-    // add chat to Firebase
     
+    // add chat to Firebase
+    database.ref().push(chat);
     
   });
   
