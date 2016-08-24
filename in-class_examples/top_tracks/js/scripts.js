33464e2d1b1fdc0b32d3d86b281952145742d6d9
Christoph Anderson <cwolfanderson@gmail.com>
2016-08-24 10:11:00 -0700

    added functionality

--- in-class_examples/top_tracks/js/scripts.js
+++ in-class_examples/top_tracks/js/scripts.js
@@ -1,72 +1,86 @@
 $(function() {
   
-  $('#compare-btn').click(function() {
-    
-    // get artists from input
-    let artist1 = $('#artist1').val();
-    let artist2 = $('#artist2').val();
-    
-    // make AJAX request to get artist id
-    let artist1Id = getArtistId(artist1);
-    let artist2Id = getArtistId(artist2);
-    
-    console.log(artist1Id);
-    console.log(artist2Id);
-    
-    let $artist1Tracks = $('#artist1-tracks');        
-    // console.log($artist1Tracks);
-    
-    let $artist2Tracks = $('#artist2-tracks');    
-    // let artist1Tracks = document.getElementById('#artist1-tracks');   
-    
-    setTimeout(function() {
-      displayTracks(artist1Id, $artist1Tracks);
-      displayTracks(artist2Id, $artist2Tracks);
-    }, 3000);
-    
+  let $artist1 = $('#artist1');
+  let $artist2 = $('#artist2');
+  
+  $('#compare-btn').click(start);
+  
+  $artist1.keypress(function(event) {
+    if (event.key === 'Enter') start();
   });
   
-  function displayTracks(id, $el) { 
+  $artist2.keypress(function(event) {
+    if (event.key === 'Enter') start();
+  });
+  
+  function start() {
+    
+    $('#compare-btn').addClass('pulse');
+    
+    // clear artist tracks
+    let $artist1Tracks = $('#artist1-tracks');
+    let $artist2Tracks = $('#artist2-tracks');    
+    
+    $artist1Tracks.empty();
+    $artist2Tracks.empty();
+    
+    // get artists from input
+    let artist1 = $artist1.val();
+    let artist2 = $artist2.val();
+    
+    // make AJAX request to get artist id
+    // let artist1Id = getArtistId(artist1);
+    
+    
+    init(artist1, $artist1Tracks);
+    init(artist2, $artist2Tracks);
+    
+    function init(artistName, $el) {
+      
+      $.getJSON(`https://api.spotify.com/v1/search?q=${artistName}&type=artist`).then(function(artistData) {
+        // console.log(artistData.artists.items[0].id);
+        let id = artistData.artists.items[0].id;
+        let name = artistData.artists.items[0].name;
+        
+        displayTracks(name, id, $el);      
+        
+      });
+    }
+    
+  }
+  
+  function displayTracks(name, id, $el) { 
+    
+    $el.append(`<h1>${name}</h1>`);
     
     // use ids to get top tracks
     $.ajax({
       url: `https://api.spotify.com/v1/artists/${id}/top-tracks?country=us`,
       success: function(topTracks) {
         let tracks = topTracks.tracks;
-        let name, imgUrl;
+        let name, imgUrl, audioUrl;
         tracks.forEach(function(track) {
           
           name = track.name;
           imgUrl = track.album.images[1].url;
+          audioUrl = track.preview_url;
           
-          let nameTag = `<h3>${name}</h3>`
-          let imgTag = `<img src=${imgUrl} alt=${artist1}>`;
-          // add name & img to artist-tracks          
-          $el.append(nameTag + imgTag);
+          // add stuff to html
+          $el.append(`
+            <div class="animated zoomInDown">
+            <h3>${name}</h3>
+            <img src=${imgUrl} alt=${artist1}>
+            <audio controls>
+            <source src="${audioUrl}" type="audio/mpeg">
+            Your browser does not support the audio element. Get Chrome!
+            </audio>
+            </div>
+          `);
           
         });
         
       }
     });
-    // }, 3000);
-    
-  }
-  
-  function getArtistId(name) {
-    let id;
-    
-    // $.when( 
-      $.ajax({      
-        url: `https://api.spotify.com/v1/search?q=${name}&type=artist`,
-        async: false,
-        success: function(artistData) {
-          console.log(artistData.artists.items[0].id);
-          id = artistData.artists.items[0].id;        
-        }
-      })
-    // ).done(function() {
-      return id;      
-    // });
     
   }
   
