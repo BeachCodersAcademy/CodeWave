1883a2b1660339295d71e282f5191347a19659f7
Christoph Anderson <cwolfanderson@gmail.com>
2016-08-24 11:40:49 -0700

    Initial upload

---
+++ in-class_examples/chatty_cathy/js/chatty_cathy.js
@@ -0,0 +1,19 @@
+$(function() {
+  
+  
+  $('#chat-btn').click(function() {
+
+    let $chat = $('textarea');        
+    let chat = $chat.val();        
+    $chat.val('');
+    
+    $chat.focus();
+
+    // add chat to Firebase
+    
+    
+  });
+  
+  
+  
+});
