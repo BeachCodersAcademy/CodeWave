ce40e69a67ff4f3e6e04b7a26561e18a4f789b50
marcllopis <marcllopis89@gmail.com>
2016-10-26 12:38:33 -0700

    loading gif added + no match found message + convert to lb & feet

--- in-class_examples/october-2016/Cloud City Love/js/scripts.js
+++ in-class_examples/october-2016/Cloud City Love/js/scripts.js
@@ -34,9 +34,13 @@ let $skinColor = $('#skin-color');
 let $matchesContainer = $('#matches-container');
 
 $('#find-love-btn').click(() => {
-  
+
+  // load the .gif image when the user clicks on button and append it to the #waiting div
+  let imgWait=`<img src="lib/assets/img/loading/bb8.gif" class="wait"><h5 class=text-center>Finding your love!</h5>`;
+  $('#waiting').append(imgWait);
+
   $matchesContainer.empty();
-  
+
   let fromHeight = $fromHeight.val() ? Number($fromHeight.val()) : 0;
   let toHeight = $toHeight.val() ?  Number($toHeight.val()) : Number.POSITIVE_INFINITY;
   let fromWeight =  Number($fromWeight.val()) ? $fromWeight.val() : 0;
@@ -46,90 +50,120 @@ $('#find-love-btn').click(() => {
   let gender = $gender.val();
   let hairColor = $hairColor.val() ? $hairColor.val() : 'any';
   let skinColor = $skinColor.val() ? $skinColor.val() : 'any';
-  
+
   let matches = [];
-  
+
   for (let i = 1; i <= 9; i++) {
-    
+
     $.getJSON('http://swapi.co/api/people/?format=json&page=' + i, data => {
-      
+
       let people = data.results;
-      
+
       // console.log(people);
       // check for matching inputs
-      
+
       people.forEach(person => {
         if (person.height >= fromHeight && person.height <= toHeight
           && person.mass >= fromWeight && person.mass <= toWeight) {
-            
-            // check gender (male, female, or n/a)            
+
+            // check gender (male, female, or n/a)
             if (gender === 'any'
             || gender === person.gender) {
-              
+
               // check hair color
               if (hairColor === 'any'
               || person.hair_color.includes(hairColor)) {
-                
+
                 // check skin color
                 if (skinColor === 'any'
                 || person.skin_color.includes(skinColor)) {
-                  
+
                   matches.push(person);
-                  
+
                 } // / skin color
-                
+
               } // / hair color
-              
+
             } // / gender
-            
+
           } // / height & weight
-          
+
         }); // / forEach
-        
+
       }); // / getJSON
-      
+
     }; // / for
-    
+
     setTimeout(() => {
-      
+
+      //when we print the results we don't want the .gif image anymore, so we empty it
+      $('#waiting').empty(imgWait);
+
       console.log(matches);
-      
-      // put matches on screen
-      matches.forEach(person => {
-        
+
+      // need to check if matches is empty to display something like "No match was found"
+
+      if(matches.length === 0){
+        $matchesContainer.append(`
+          <h3 class="no-match-title">Not even in a galaxy far far away there's a match for you!</h3>
+          <p class="no-match-text">Try changing some of your preferences</p>
+        `);
+      }else { //if not empty, we display the results
+
+        // put matches on screen
+        matches.forEach(person => {
+
         let name = person.name;
         // TODO: get image url
-        // let imgURL = 
+        // let imgURL =
         let gender = person.gender;
         let height = person.height;
         let weight = person.mass;
         let hairColor = person.hair_color;
         let skinColor = person.skin_color;
-        
+
+        let weightLb = kilogramsToPounds(weight);
+        let heightFeet = cmToFeet(height);
+
         $matchesContainer.append(`
           <h2>${name}</h2>
           <p><small>${gender}</small></p>
           <img src="" alt="${name}" />
-          <p>Height: ${height} | Weight: ${weight}</p>
+          <p>Height: ${heightFeet} | Weight: ${weightLb} lb</p>
           <p>Hair Color: ${hairColor} | Skin Color: ${skinColor}</p>
         `);
-        
-      });
-      
-    }, 3000);
-    
-    
-    
+
+        });
+      }
+
+    }, 4000); //Change it to 4s cause it wasn't finding everyone sometimes
+
+
+
   });
-  
+
+//convert kg to lb
+  function kilogramsToPounds(n) {
+      return (n/0.4536).toFixed(1);
+  }
+
+//convert cm to feet and inches
+  function cmToFeet(n) {
+    var realFeet = ((n*0.393700) / 12);
+    var feet = Math.floor(realFeet);
+    var inches = Math.round((realFeet - feet) * 12);
+    return feet + "&prime;" + inches + '&Prime;';
+  }
+
+
   /*
   CRUD Operations
-  
+
   Create -> POST
   Read -> GET
   Update -> PUT
   Delete -> REMOVE
-  
+
   Strongly Typed Languages
-  Javascript -> Weakly Typed  
+  Javascript -> Weakly Typed
   */
