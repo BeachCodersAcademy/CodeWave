d9861237be11bb5b4adc69c91b5c5d04429e4acd
Christoph Anderson <CWolfAnderson@gmail.com>
2016-11-17 18:35:58 -0800

    Update 04-event-listeners-and-object.js

--- notes/react/04-event-listeners-and-object.js
+++ notes/react/04-event-listeners-and-object.js
@@ -1,11 +1,5 @@
-/*
-Event listeners in JSX are camel-cased instead of lower case or dashed.
-*/
-
-let ReactDOM = require('react-dom');
-
 let isCat = true;
-let catUrl = 'http://kurld.com/images/wallpapers/cute-cats/cute-cats-22.jpg';
+let catUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Indian_Cat_pic.jpg/220px-Indian_Cat_pic.jpg';
 let dogUrl = 'https://img.buzzfeed.com/buzzfeed-static/static/2015-07/10/15/campaign_images/webdr14/secrets-doggy-daycare-employees-will-never-tell-y-2-4088-1436557195-1_dblbig.jpg';
 
 function swapImage(e) {
