30f0afeeb3d8c0da97433dc8ce18201b525b8c8d
Christoph Anderson <CWolfAnderson@gmail.com>
2016-11-28 19:27:56 -0800

    Update webpack.config.js

--- notes/webpack.config.js
+++ notes/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         test: /.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot-loader/webpack', 'babel?presets[]=react,presets[]=es2015']
+        loaders: ['babel?presets[]=react,presets[]=es2015']
       }
     ]
   },
